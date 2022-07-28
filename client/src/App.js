178f790db15974fcab0b4d07bc76fe809b42b9ab
frank-booth <jfraley519@gmail.com>
2022-07-28 18:50:47 -0400

    added sound to the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Nav from './components/Nav'
 import Home from './components/Home'
 import Bonds from './components/Bonds'
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,23 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
+  const [toggle, setToggle] = useState(false)
+
+  const audio = new Audio(
+    'https://ia600908.us.archive.org/14/items/tvtunes_6995/James%20Bond%20%28Original%29.mp3'
+  )
+
+  const start = () => {
+    audio.play()
+  }
+
   return (
     <div className="homepage">
-      <img className="bondGif" src="https://i.gifer.com/KYAg.gif" />
+      <img
+        className="bondGif"
+        onClick={start}
+        src="https://i.gifer.com/KYAg.gif"
+      ></img>
     </div>
   )
 }
